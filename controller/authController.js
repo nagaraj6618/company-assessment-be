a364c55f9e6e46b941a55f9a2530f30b1226ae6e
NAGARAJ S <nagaraj516700@gmail.com>
2024-04-30 21:49:28 +0530

    Feat : Register Controller added

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,28 @@
 const userSchema = require('../model/userModel.js');
+const bcrypt = require('bcryptjs');
 
 async function registerUser (req,res) {
-   const data = await userSchema.find();
-   console.log(req.body);
-   res.status(200).json({message:"Register Method",data:data});
+   // const data = await userSchema.find();
+   try{
+      const userData = req.body;
+      const salt = bcrypt.genSaltSync(10);
+      const hashPassword = bcrypt.hashSync(req.body.password,salt);
+      userData.password = hashPassword;
+      const newUser = new userSchema(userData)
+      console.log(newUser);
+      await newUser.save();
+      res.status(200).json({message:"Register Successfull",data:newUser});
+      
+
+   }
+   catch(error){
+      if(error.errorResponse.code){
+         return res.status(400).json({message:"Account already exist",error:error})
+      }
+      
+      res.status(500).json({message:"Server Crashed",error:error});
+   }
+   
 
 }
 
