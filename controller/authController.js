284a23939470389894aba0b23bb6c8e6113437c7
NAGARAJ S <nagaraj516700@gmail.com>
2024-04-30 22:15:46 +0530

    Feat : Added Email validation using API

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,27 @@
 const userSchema = require('../model/userModel.js');
 const bcrypt = require('bcryptjs');
+const axios = require('axios');
 
+async function emailValidationChecker (email){
+   try{
+   const key = process.env.EMAIL_VALIDATE_API_KEY;
+   const response = await axios.get(`https://emailvalidation.abstractapi.com/v1/?api_key=${key}&email=${email}`);
+   console.log(response.data.deliverability)
+   return response.data.deliverability == "DELIVERABLE"
+   }
+   catch(error){
+      return false;
+   }
+
+   
+}
 async function registerUser (req,res) {
    // const data = await userSchema.find();
    try{
+      const isValid = await emailValidationChecker(req.body.email);
+      if(!isValid){
+         return res.status(400).json({message:"Email is not valid"});
+      }
       const userData = req.body;
       const salt = bcrypt.genSaltSync(10);
       const hashPassword = bcrypt.hashSync(req.body.password,salt);
@@ -12,7 +30,7 @@ async function registerUser (req,res) {
       console.log(newUser);
       await newUser.save();
       res.status(200).json({message:"Register Successfull",data:newUser});
-      
+   
 
    }
    catch(error){
